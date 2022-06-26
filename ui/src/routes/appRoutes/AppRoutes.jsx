007cf71fdaa1ce046356dbfbf28e09155b4dbe25
usamarajpoot03 <usamarajpoot03@yahoo.com>
2022-06-26 23:54:06 +0500

    navbar integrated

--- ui/src/routes/appRoutes/AppRoutes.jsx
+++ ui/src/routes/appRoutes/AppRoutes.jsx
@@ -1,17 +1,25 @@
 import React, { Suspense, lazy } from "react";
 import { Switch, Redirect } from "react-router-dom";
 import { AuthRoute } from "../protectedRoute/ProtectedRoute";
-import { CircularProgress, Grid } from "@material-ui/core";
+import { CircularProgress, Grid, makeStyles } from "@material-ui/core";
 const Home = lazy(() => import("../../containers/home/Home"));
 const Test = lazy(() => import("../../containers/test/Test"));
 const Result = lazy(() => import("../../containers/result/Result"));
 
+const useStyles = makeStyles((theme) => ({
+  fallback: {
+    margin: theme.spacing(8),
+  },
+}));
+
 const AppRoutes = ({ user }) => {
+  const classes = useStyles();
+
   return (
     <Suspense
       fallback={
-        <Grid container justifyContent="center">
-          <CircularProgress size={50} />
+        <Grid container justifyContent="center" className={classes.fallback}>
+          <CircularProgress size={40} />
         </Grid>
       }
     >
